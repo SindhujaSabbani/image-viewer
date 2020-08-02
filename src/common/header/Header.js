2c0c917a73923db4662371bd6147d4c00447d23a
Sindhuja <sindhuja.sabbani@gmail.com>
2020-08-02 15:23:44 +0530

    Implemented profile menu

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,7 +5,10 @@ import Input from '@material-ui/core/Input';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
-
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import {Link} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 
 
 class Header extends Component {
@@ -13,13 +16,35 @@ class Header extends Component {
     constructor() {
         super();
 
-        console.log("xx" + sessionStorage.getItem('access-token') + "xx" )
+        console.log("xx" + sessionStorage.getItem('access-token') + "xx")
         this.state = {
+            menuOpen: false,
             searchBox: sessionStorage.getItem('access-token') == null ? "search-box-none" : "search-box-show"
         };
 
 
     }
+
+    handleOpenMenu = () => {
+        this.setState({menuOpen: true})
+    }
+
+    getProfileButton = () => {
+        document.getElementById("profile-icon")
+    }
+    handleMyAccount = () => {
+        this.setState({menuOpen: false})
+    }
+    handleLogout = () => {
+        this.setState({menuOpen: false})
+        sessionStorage.removeItem("access-token");
+    }
+
+
+    handleClose = () => {
+        this.setState({menuOpen: false})
+    }
+
     render() {
         return (
             <div className="flex-container header">
@@ -27,11 +52,33 @@ class Header extends Component {
                 <div className={this.state.searchBox}>
                     <div className="search">
                         <SearchIcon></SearchIcon>
-                        <Input id="search" placeholder='Search...' type="text"  disableUnderline={true}/>
+                        <Input id="search" placeholder='Search...' type="text" disableUnderline={true}/>
                     </div>
-                    <IconButton >
-                        <Avatar src='batmanprofile.jpg'/>
+                    <IconButton id="profile-icon">
+                        <Avatar src='batmanprofile.jpg' onClick={this.handleOpenMenu}/>
                     </IconButton>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={this.getProfileButton}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        open={this.state.menuOpen}
+                        onClose={this.handleClose}>
+
+
+                        <MenuItem component={Link} to='/profile' onClick={this.handleMyAccount}>My account</MenuItem>
+                        <hr/>
+                        <MenuItem component={Link} to='/' onClick={this.handleLogout}>Logout</MenuItem>
+
+                    </Menu>
                 </div>
             </div>
         )
